2c0b730b1ff256bd06bdcd1c6314ec58482b80c2
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-02-03 19:30:40 +0530

    add Ui design to AboutScene

--- src/main/java/lk/ijse/dep13/lume/controller/AboutController.java
+++ src/main/java/lk/ijse/dep13/lume/controller/AboutController.java
@@ -1,4 +1,12 @@
 package lk.ijse.dep13.lume.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class AboutController {
+    public void lblLicenseOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void btnGitHubOnAction(ActionEvent actionEvent) {
+    }
 }
