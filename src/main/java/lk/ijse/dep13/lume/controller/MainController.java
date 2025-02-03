11bddfabf80b303635d172501ab0b409da17a2bb
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-02-03 20:33:40 +0530

    add validation for URL

--- src/main/java/lk/ijse/dep13/lume/controller/MainController.java
+++ src/main/java/lk/ijse/dep13/lume/controller/MainController.java
@@ -1,8 +1,10 @@
 package lk.ijse.dep13.lume.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -19,7 +21,48 @@ public class MainController {
     public WebView wbDisplay;
     public Button btnAbout;
 
+    private String url;
+
+    public void initialize() {
+        // initially URL set to http://google.com
+        txtAddress.setText("http://google.com/");
+
+        txtAddress.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) Platform.runLater(txtAddress::selectAll);
+        });
+    }
+
     public void txtAddressOnAction(ActionEvent actionEvent) {
+        url = txtAddress.getText();
+        if (url == null || url.isEmpty()) showAlert(Alert.AlertType.WARNING, "WARNING", "URL can't be empty");
+        if (validateAddress(url)) {
+            getInfoUrl(url);
+        } else {
+            showAlert(Alert.AlertType.ERROR, "ERROR", "Invalid URL");
+        }
+    }
+
+    private void getInfoUrl(String url) {
+        
+    }
+
+    private boolean validateAddress(String address) {
+        if (url == null || url.isEmpty()) return false;
+        if (url.startsWith("http://") || url.startsWith("https://")) return true;
+        if (!url.contains("://") && url.contains(".")) return true;
+        if (url.contains("://")) {
+            int colonIndex = url.indexOf(":", url.indexOf("://") + 3);
+            if (colonIndex > 0) return true;
+            return false;
+        }
+        else return false;
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
     public void btnAboutOnAction(ActionEvent actionEvent) throws IOException {
