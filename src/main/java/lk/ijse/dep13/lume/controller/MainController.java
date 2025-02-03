8adbdfa3350a7ea01941db216201a1635db34d0e
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-02-03 19:32:48 +0530

    create aboutbtn functionality

--- src/main/java/lk/ijse/dep13/lume/controller/MainController.java
+++ src/main/java/lk/ijse/dep13/lume/controller/MainController.java
@@ -1,10 +1,17 @@
 package lk.ijse.dep13.lume.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class MainController {
     public AnchorPane root;
@@ -15,6 +22,13 @@ public class MainController {
     public void txtAddressOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnAboutOnAction(ActionEvent actionEvent) {
+    public void btnAboutOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage(StageStyle.UTILITY);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.centerOnScreen();
+        stage.resizableProperty().setValue(false);
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/scene/About.fxml")));
+        stage.setScene(scene);
+        stage.show();
     }
 }
